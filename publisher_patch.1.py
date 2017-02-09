df14d95287964288d950e248655e7e485ca5ff1f
vmanamino <vmanamino@gmail.com>
2017-02-09 18:14:51 +0000

    script for publisher

--- publisher_patch.1.py
+++ publisher_patch.1.py
@@ -20,39 +20,48 @@ assessing = "767c92cc-8a1f-468d-bf07-e46f2c3a7b0e"
 
 owner_exceptions = [bostonMaps, bwsc, assessing]
 
-new_owner = "innovation-and-technology"
+new_owner = "department-of-innovation-and-technology-org"
 
-updates = report_library.batch_report('owner_org')
-errors = report_library.error_report('owner_org')
-exceptions = report_library.exception_report('owner_org')
-no_keys = report_library.no_key_report('owner_org')
+
+
+updates = report_library.batch_report('publisher')
+errors = report_library.error_report('publisher')
+exceptions = report_library.exception_report('publisher')
 
 test_count = 3
 count = 0
 if not packages == "none":
     items = 0
+    updates.write('name\tcontact email\tprovider\n')
+    exceptions.write('name\tprovider\tcontact email\n')
     for name in packages:
-        exception = False
         data = ckan_api_library.package(name)
-        if data['content'].has_key('owner_org'):
-            for owner in owner_exceptions:
-                if data['content']['owner_org'] == owner:
-                    exception = True
-                    exceptions.write('%s\t%s\n' % (name, data['content']['owner_org']))
-                    items += 1
-                    break
-            if not exception:
-                payload = {"id": name, "owner_org": new_owner}
+        if data['content'].has_key('contact_point_email'):
+            if not data['content']['contact_point_email'] == "EGIS_Team@boston.gov":
+                payload = {"id": name, "publisher": "Department of Innovation and Technology"}
                 response = ckan_api_library.package_patch_request(payload)
                 if response['code'] == 200:
                     updates.write('%s\t%s\t%s\n' % (name, data['content']['owner_org'], response['code']))
                     items += 1
                 else:
                     errors.write('%s\t%s\n' % (name, response['code']))
+                
+            else:
+                exceptions.write('%s\t%s\n' % (data['content']['name']
+                    , data['content']['owner_org']))
         else:
-            no_keys.write('%s\n' % (name))
-            items += 1
-          
+            if data['content'].has_key('owner_org'):
+                payload = {"id": name, "publisher": "Department of Innovation and Technology"}
+                response = ckan_api_library.package_patch_request(payload)
+                if response['code'] == 200:
+                    updates.write('%s\t%s\t%s\n' % (name, data['content']['owner_org'], response['code']))
+                    items += 1
+                else:
+                    errors.write('%s\t%s\n' % (name, response['code']))
+            else:
+                exceptions.write('%s\tnone\tnone\n' % (data['content']['name']))
+                
+        
     if len(packages) == items:
         print('job done')
     else:
@@ -64,7 +73,4 @@ if not packages == "none":
             print('something is screwy!')
 else:
     print("no packages")
-
-
-
 

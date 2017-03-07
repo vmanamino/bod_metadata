e074c3c3a7f96e71d007b09f077ea5cef9821503
vmanamino <vmanamino@gmail.com>
2017-03-07 19:32:54 +0000

    updated script

--- legacy_desc.py
+++ legacy_desc.py
@@ -1,6 +1,6 @@
 import ckan_api_library
 import report_library
-import json
+import json, os
 import sys
 import urllib2
 import urllib
@@ -10,23 +10,32 @@ sys.setdefaultencoding('utf-8')
 
 
 def get_sheet():
-    request = "https://sheetsu.com/apis/v1.0/ab719a3ecb64"
+    request = os.environ['GOOGLE_SHEET_API']
     try:
         return urllib2.urlopen(request)
     except urllib2.HTTPError as err:
         return err
 
 response = get_sheet()
-
+count = 0
 if response.code == 200:
+    report = report_library.batch_report("legacy_desc")
+    error = report_library.error_report("legacy_desc")
     data = json.loads(response.read())
     for title in data:
-        if title['Order of upload']:
+        if title['Status'] == "Ready for upload":
             eyedee = title['NAME']
             new_desc = title['NEW DESC']
             payload = {"id": eyedee, "notes": new_desc, 
             "notes_translated": {"en": new_desc}}
             response = ckan_api_library.package_patch_request(payload)
-            print(response['code'])
-
-
+            if response['code'] == 200:
+                print(response['code'])
+                print(title['NAME'])
+                count += 1
+                report.write("%s\n" % (title['NAME']))
+            else:
+                print(response['code'])
+                print(title['NAME'])
+                error.write("%s\n" % (title['NAME']))
+    report.write("%s datasets updated with descriptions\n" % count)
